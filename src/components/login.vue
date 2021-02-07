802586cf366606a49cbc46cd8b202b9911f2c607
chenyong <840658280@qq.com>
2021-02-07 15:11:40 +0800

    新初始化

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+   dsfdfs
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,24 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
+
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
+import test from '@/components/test'
 
 Vue.use(Router)
 
@@ -8,8 +26,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login
+    },
+    {
+      path:'/test',
+      component:test
     }
   ]
-})
+}
+)
